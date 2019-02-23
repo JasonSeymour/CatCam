e8384b3468151d90b3f1d1918de69102be9b3898
JasonSeymour <jaseymour@gmail.com>
2019-02-23 17:17:05 -0600

    Fetch_And_Transfer:   - added call out to ImageMagic convert to insert timestamp into image

--- fetch_and_transfer.py
+++ fetch_and_transfer.py
@@ -3,6 +3,7 @@
 import requests
 import shutil
 import paramiko
+import subprocess
 
 reqobj = requests.get('http://192.168.1.56:8080/shot.jpg', stream=True)
 if reqobj.status_code == 200:
@@ -10,7 +11,10 @@ if reqobj.status_code == 200:
         reqobj.raw.decode_content = True
         shutil.copyfileobj(reqobj.raw, f)
         f.close()
-        
+
+cmd = '/usr/bin/convert /home/pi/image.jpg -pointsize 20 -fill red -gravity northwest -annotate +0+0 "$(date)" /home/pi/image2.jpg'
+subprocess.call(cmd, shell=True)
+
 paramiko.util.log_to_file('/tmp/catcam_transfer.log')
 host = "catcam.theseymours.org"
 port = 22
@@ -19,7 +23,7 @@ transporter = paramiko.Transport((host,port))
 rsa_key = paramiko.RSAKey.from_private_key_file('/home/pi/.ssh/catcam.pem')
 transporter.connect(username=user, pkey=rsa_key)
 sftp = paramiko.SFTPClient.from_transport(transporter)
-localpath = '/home/pi/image.jpg'
+localpath = '/home/pi/image2.jpg'
 remotepath = '/var/html/image.jpg'
 sftp.put(localpath,remotepath)
 sftp.close()
