4e648902d624e80babc582f72a710123467d9a32
JasonSeymour <jaseymour@gmail.com>
2019-02-23 16:35:05 -0600

    Fetch_And_Transfer:   - Added sftp transfer code   - All functions appear to be working correctly

--- fetch_and_transfer.py
+++ fetch_and_transfer.py
@@ -2,6 +2,7 @@
 
 import requests
 import shutil
+import paramiko
 
 reqobj = requests.get('http://192.168.1.56:8080/shot.jpg', stream=True)
 if reqobj.status_code == 200:
@@ -10,4 +11,16 @@ if reqobj.status_code == 200:
         shutil.copyfileobj(reqobj.raw, f)
         f.close()
         
-        
+paramiko.util.log_to_file('/tmp/catcam_transfer.log')
+host = "catcam.theseymours.org"
+port = 22
+user = 'ec2-user'
+transporter = paramiko.Transport((host,port))
+rsa_key = paramiko.RSAKey.from_private_key_file('/home/pi/.ssh/catcam.pem')
+transporter.connect(username=user, pkey=rsa_key)
+sftp = paramiko.SFTPClient.from_transport(transporter)
+localpath = '/home/pi/image.jpg'
+remotepath = '/var/html/image.jpg'
+sftp.put(localpath,remotepath)
+sftp.close()
+transporter.close()
